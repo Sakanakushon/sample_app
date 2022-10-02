5273380c6471cb65acac06e6eb77dcd29dd06752
Sakanakushon <110516513+Sakanakushon@users.noreply.github.com>
2022-10-02 13:18:21 +0000

    [Finish]3-10

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,10 +6,14 @@ class ListsController < ApplicationController
   # 以下を追加
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    redirect_to list_path(list.id)
+    if @list.save
+     redirect_to list_path(@list.id)
+    else
+     @lists = List.all    
+     render :new
+    end
   end
   
   def index
